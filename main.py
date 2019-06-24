be1da68799faebc07731a7c4f75d9e375beaa0aa
steplyakov <steplyakov@cloudlinux.com>
2019-06-24 18:06:48 +0700

    Added model processing, synchronous parsing

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
 import asyncio
 import aiohttp
 import requests as re
+from model import Pages, Links
+import lxml.html as html
+
 
 MAX_DEPTH = 4
+MAIN_DOMAIN = 'https://ru.wikipedia.org'
 loop = asyncio.get_event_loop()
 
 
-def update_page_table(url, request_depth):
-    pass
+def update_page_table(url, depth):
+    result = Pages.add_page(url, depth)
+    if result:
+        return True
 
 
 def update_links_table(parent_url, current_url):
-    pass
+    Links.add_link(parent_url, current_url)
 
 
-def parse_url(url, depth=1, parent=None):
-    pass
-
+def parse_page(parent_url, depth=1):
+    '''
+    Get all urls from article ignoring unexisting urls (starts with /w/) and
+    non-article urls (everything not in "div id='bodyContent'")
+    :param parent_url: url to be parsed
+    :param depth: current parse depth
+    :return:
+    '''
+    r = re.get(parent_url)
+    tree = html.fromstring(r.text)
+    subtree = tree.xpath('//div[@id="bodyContent"]')[0]
+    url_list = [i for i in subtree.xpath("*//a/@href") if i.startswith('/wiki')]
+    current_depth = depth + 1
+    for url in url_list:
+        current_url = MAIN_DOMAIN + url
+        exists = update_page_table(current_url, current_depth)
+        update_links_table(parent_url, current_url)
+        if not exists and current_depth != MAX_DEPTH:
+            parse_page(current_url, current_depth)
--- model.py
+++ model.py
@@ -11,6 +11,13 @@ class Pages(Model):
     url = CharField()
     request_depth = IntegerField()
 
+    @classmethod
+    def add_page(cls, url, depth):
+        q = cls.select().where(cls.url == url)
+        if not q.exists():
+            cls.create(url=url, request_depth=depth)
+            return True
+
 
 class Links(Model):
     class Meta:
@@ -18,3 +25,24 @@ class Links(Model):
         db_table = 'links'
     from_page_id = ForeignKeyField(Pages, backref='from_page')
     link_id = ForeignKeyField(Pages, backref='link')
+
+    @classmethod
+    def add_link(cls, parent_url, child_url):
+        parent_id = Pages.get(Pages.url == parent_url)
+        child_id = Pages.get(Pages.url == child_url)
+        cls.create(from_page_id=parent_id, link_id=child_id)
+
+
+db.create_tables([Pages, Links])
+# Pages.add_page('a', 1)
+# Pages.add_page('b', 2)
+# Pages.add_page('c', 2)
+# Pages.add_page('d', 3)
+# Pages.add_page('e', 3)
+# Pages.add_page('f', 3)
+#
+# Links.add_link('a', 'b')
+# Links.add_link('a', 'c')
+# Links.add_link('b', 'd')
+# Links.add_link('b', 'f')
+# Links.add_link('c', 'e')
